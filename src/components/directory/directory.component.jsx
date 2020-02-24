2d0fa837b665f642a1a32b4804e4dd581921c242
Renato Hoxha <hoxha.renato@gmail.com>
2020-02-25 00:04:27 +0100

    adding shop page, collection item and collection preview item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,28 +11,33 @@ class Directory extends React.Component {
                 id: '1',
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                linkUrl: 'hats'
             },
             {
                 id: '2',
                 title: 'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                linkUrl: 'jackets'
             },
             {
                 id: '3',
                 title: 'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                linkUrl: ''
             },
             {
                 id: '4',
                 title: 'womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                size : 'large'
+                size : 'large',
+                linkUrl: ''
             },
             {
                 id: '5',
                 title: 'mens',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                size: 'large'
+                size: 'large',
+                linkUrl: ''
             }]
         }
     }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu inner-container'>
                 {
-                    this.state.section.map(({id, title, imageUrl, size}) => (
-                        <MenuItem key={id} title={title} imageUrl ={imageUrl} size ={size}/>
+                    this.state.section.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
                 
